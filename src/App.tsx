fa519f2b877f6783233a40d6c7f5cc0ebf5a55eb
Karitham <kar@karitham.dev>
2022-06-02 16:33:11 +0200

    fix resume not available on gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,20 @@
-import { Component } from "solid-js";
+import { Component, For } from "solid-js";
 import "./App.css";
 import Main from "./components/Main";
 import Projects from "./components/Projects";
 import WhoIS from "./components/WhoIS";
 
+const components = [Main, Projects, WhoIS];
+
 const App: Component = () => {
   return (
-    <main>
-      <div class="page">
-        <Main />
-      </div>
-      <div class="page">
-        <Projects />
-      </div>
-      <div class="page">
-        <WhoIS />
-      </div>
-    </main>
+    <For each={components}>
+      {(Component) => (
+        <div class="page">
+          <Component />
+        </div>
+      )}
+    </For>
   );
 };
 
